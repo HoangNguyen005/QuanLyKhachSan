4ef007c4febba2b16aab0d8e91f8c0ac3a9a8c7c
Theanh2004-z <phantheanh.x@gmail.com>
2025-06-12 11:18:22 +0700

    .

--- QuanLyKhachSan/Program.cs
+++ QuanLyKhachSan/Program.cs
@@ -20,7 +20,7 @@ namespace QuanLyKhachSan
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             
-            Application.Run(new frmMain());
+            Application.Run(new frmLogin());
         }
     }
 }
